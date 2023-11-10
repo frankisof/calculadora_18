699821eb40ab71a59cf8c94a6d5c585b8709c29d
Francisco <fernandezfrancisco261@gmail.com>
2023-11-10 11:13:16 -0600

    coseno

---
+++ src/app/coseno/coseno.ts
@@ -0,0 +1,11 @@
+export function coseno(num1: number, num2: number) {  
+  const grados = num1 * (  Math.PI/180);     
+  const coseno = Math.cos(grados);
+       
+        return coseno;
+      
+      }
+    
+
+      
+    
